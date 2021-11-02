809d1e6792be6cbd6741b8a2ae31dfdd30c5ba12
Shubham <shubh.chavhan00@gmail.com>
2021-11-02 18:43:14 +0530

    added navigation and routes to the App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navigation from "./components/Navigation";
+import Routes from "./routes";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyle, { lightTheme, darkTheme } from "./theme/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { UseDarkMode } from "./theme/UseDarkMode";
 
-function App() {
+const App = () => {
+  const [theme, toggleTheme] = UseDarkMode();
+
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Navigation theme={theme} toggleTheme={toggleTheme} />
+        {/* <Toggle theme={theme} toggleTheme={toggleTheme} /> */}
+        <Routes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
