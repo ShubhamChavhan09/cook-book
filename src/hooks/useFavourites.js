05c1dce9fd79dcbebb7080756f5b96cc674b6d1e
Shubham <shubh.chavhan00@gmail.com>
2021-11-09 15:45:42 +0530

    hook to save favourites selected in LocalStorage

---
+++ src/hooks/useFavourites.js
@@ -0,0 +1,32 @@
+import { useStore, useDispatch } from "../context";
+
+export const useFavourites = () => {
+  const dispatch = useDispatch();
+  const { favourites } = useStore();
+
+  const getFavourites = () => {
+    let tempFavourites =
+      JSON.parse(localStorage.getItem("meal_favorites")) || [];
+
+    dispatch({ type: "FAVOURITES", payload: tempFavourites });
+  };
+
+  const setFavourites = (meal) => {
+    let tempFavourites = favourites;
+
+    if (!tempFavourites.some((fav) => fav.idMeal === meal.idMeal)) {
+      tempFavourites.push(meal);
+      localStorage.setItem("meal_favorites", JSON.stringify(tempFavourites));
+      dispatch({ type: "FAVOURITES", payload: tempFavourites });
+    } else {
+      const filteredFavs = tempFavourites.filter(
+        (fav) => fav.idMeal !== meal.idMeal
+      );
+
+      dispatch({ type: "FAVOURITES", payload: filteredFavs });
+      localStorage.setItem("meal_favorites", JSON.stringify(filteredFavs));
+    }
+  };
+
+  return { getFavourites, setFavourites };
+};
