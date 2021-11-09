9b7cd8301bbc50ba5748f8f14ca5694606750af5
Shubham <shubh.chavhan00@gmail.com>
2021-11-09 15:46:28 +0530

    Component will list all the favourites selected

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useStore } from "../context";
+import MealItems from "../components/MealItems.js";
 
 const Favorites = () => {
+  const { favourites } = useStore();
+  console.log(favourites);
   return (
-    <div>
-      <h1>favorites page</h1>
-    </div>
+    <>
+      {favourites.length ? (
+        <MealItems meals={favourites} />
+      ) : (
+        <div>No Favourites</div>
+      )}
+    </>
   );
 };
 
