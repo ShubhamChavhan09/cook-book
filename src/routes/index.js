8fc2a2e65ed23397fc3955e31ee730e1a1f8bffb
Shubham <shubh.chavhan00@gmail.com>
2021-11-16 16:28:18 +0530

    added padding and text align

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ import MealDetail from "../pages/MealDetail";
 import CategoriesItems from "../pages/CategoriesItems";
 import NotFound from "../pages/404";
 import { useFavourites } from "../hooks/useFavourites";
+import styled from "styled-components";
 
 const Routes = () => {
   const { getFavourites } = useFavourites();
@@ -18,17 +19,24 @@ const Routes = () => {
   }, []);
 
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/categories" exact component={Categories} />
-      <Route path="/categories/:id" exact component={CategoriesItems} />
-      <Route path="/categories/:id/:id" exact component={MealDetail} />
-      <Route path="/favorites" exact component={Favorites} />
-      <Route path="/randomMeal" exact component={Random} />
-      <Route exact path="/:id" component={MealDetail} />
-      <Route component={NotFound} />
-    </Switch>
+    <Box>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/categories" exact component={Categories} />
+        <Route path="/categories/:id" exact component={CategoriesItems} />
+        <Route path="/categories/:id/:id" exact component={MealDetail} />
+        <Route path="/favorites" exact component={Favorites} />
+        <Route path="/randomMeal" exact component={Random} />
+        <Route exact path="/:id" component={MealDetail} />
+        <Route component={NotFound} />
+      </Switch>
+    </Box>
   );
 };
 
 export default Routes;
+
+const Box = styled.div`
+  padding: 20px;
+  text-align: center;
+`;
