c2fd9df7e3fa7c841f1a9106293a85da31b9c74a
Shubham <shubh.chavhan00@gmail.com>
2021-11-16 16:31:00 +0530

    changed color for the theme

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -7,24 +7,31 @@ const GlobalStyle = createGlobalStyle`
 
 body{
     margin: 0;
+    padding: 0;
     font-family: 'Poppins', sans-serif;
     background: ${({ theme }) => theme.body};
     color:${({ theme }) => theme.text};
-    min-width: 98.6vw;
-    transition: all 0.5s linear;
-
-  
+    width: 98.5vw;
+    transition: all 0.2s linear;
 }
 `;
 
 export default GlobalStyle;
 
 export const lightTheme = {
-  body: "#F7F7F7  ",
-  text: " #0D0D0D",
+  // body: "linear-gradient(to right, #EAEDE5 , #FFFFFF)",
+  body: "linear-gradient(to right  , #dfe4ea, #f5f6fa, #dfe4ea)",
+  // body: "#002A27",
+  text: "#0D0D0D",
+  nav: "background: linear-gradient(to bottom, #1e272e, #ecf0f1);",
 };
 
 export const darkTheme = {
-  body: " #06080E",
+  body: "linear-gradient(to right , #101010 , #2F2E33, #101010)",
+  // body: "#1A191B",
   text: "#f5f5f5",
+  nav: "background: linear-gradient(to bottom, #ecf0f1, #1e272e);",
 };
+
+// 1A191B
+// 2F2E33
