43cf0911fd2e1f9a6a44d09e07cbd283c2d4affc
Shubham <shubh.chavhan00@gmail.com>
2021-11-19 11:52:16 +0530

    changed width to 100vw

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -11,7 +11,7 @@ body{
     font-family: 'Poppins', sans-serif;
     background: ${({ theme }) => theme.body};
     color:${({ theme }) => theme.text};
-    width: 98.5vw;
+    width: 100vw;
     transition: all 0.2s linear;
 }
 `;
