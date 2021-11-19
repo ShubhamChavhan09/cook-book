7e700cc973e769440e7d3966309709e6e366c153
Shubham <shubh.chavhan00@gmail.com>
2021-11-19 16:56:52 +0530

    removed the components from Home and kept only searchbox

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,58 +4,61 @@ import styled from "styled-components";
 import MealItems from "../components/MealItems.js";
 
 const Home = () => {
-  const [meals, setMeals] = useState("");
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  // const [meals, setMeals] = useState("");
+  // const [search, setSearch] = useState("");
+  // const [query, setQuery] = useState("");
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    fetchMeal(setMeals);
-    //    eslint-disable-next-line
-  }, [query]);
+  // useEffect(() => {
+  //   fetchMeal(setMeals);
+  //   //    eslint-disable-next-line
+  // }, [query]);
 
-  const fetchMeal = async () => {
-    setIsLoading(true);
-    await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.meals);
-        setMeals(data.meals);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  };
+  // const fetchMeal = async () => {
+  //   setIsLoading(true);
+  //   await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data.meals);
+  //       setMeals(data.meals);
+  //       setIsLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error.message);
+  //     });
+  // };
 
-  if (isLoading) return <h1>Loading...</h1>;
+  // if (isLoading) return <h1>Loading...</h1>;
 
-  const handleChange = (e) => {
-    setSearch(e.target.value);
-  };
+  // const handleChange = (e) => {
+  //   setSearch(e.target.value);
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch("");
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setQuery(search);
+  //   setSearch("");
+  // };
 
   return (
-    <Wrapper>
-      <SearchMeal
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-        search={search}
-      />
-      {meals ? (
-        <div>
-          {query ? ` Results for '${query}'` : ""}
-          <MealItems meals={meals} />
-        </div>
-      ) : (
-        <div>No results for "{query}"</div>
-      )}
-    </Wrapper>
+    // <Wrapper>
+    //   <SearchMeal
+    //     handleChange={handleChange}
+    //     handleSubmit={handleSubmit}
+    //     search={search}
+    //   />
+    //   {meals ? (
+    //     <div>
+    //       {query ? ` Results for '${query}'` : ""}
+    //       <MealItems meals={meals} />
+    //     </div>
+    //   ) : (
+    //     <div>No results for "{query}"</div>
+    //   )}
+    // </Wrapper>
+    <>
+      <SearchMeal />
+    </>
   );
 };
 
