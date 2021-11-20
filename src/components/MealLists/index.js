6be34651d70dffa0e9352c80aa95cf665f54b200
Shubham <shubh.chavhan00@gmail.com>
2021-11-20 21:26:49 +0530

    Added media query for different screen sizes

--- src/components/MealLists/index.js
+++ src/components/MealLists/index.js
@@ -26,71 +26,26 @@ const MealLists = ({ meals }) => {
 
 export default MealLists;
 
-// const Container = styled.div`
-//   width: 100%;
-//   display: grid;
-//   grid-template-columns: repeat(5, 1fr);
-//   grid-gap: 80px;
-//   padding: 40px 0;
-//   img {
-//     width: 100%;
-
-//     @media (max-width: 500px) {
-//       width: 100%;
-//     }
-//   }
-//   @media (max-width: 1300px) {
-//     grid-template-columns: repeat(5, 1fr);
-//   }
-
-//   @media (max-width: 1104px) {
-//     grid-template-columns: repeat(4, 1fr);
-//   }
-//   @media (max-width: 900px) {
-//     grid-template-columns: repeat(3, 1fr);
-//   }
-//   @media (max-width: 500px) {
-//     width: 90%;
-//     grid-template-columns: repeat(2, 1fr);
-//   }
-// `;
-
-// const Card = styled.div`
-//   width: 150px;
-//   display: flex;
-//   flex-direction: column;
-//   position: relative;
-//   background: #ffffff;
-//   box-shadow: 0 0 10px #130f40;
-//   @media (max-width: 500px) {
-//     width: 100%;
-//   }
-// `;
-
-// const List = styled(Link)`
-//   text-decoration: none;
-//   color: #ff9f1a;
-
-//   :hover img {
-//     filter: brightness(0.75);
-//     transition: all 200ms ease;
-//   }
-//   :hover h3 {
-//     transform: scale(1.3);
-//     transition: all 200ms linear;
-//   }
-// `;
-
 const Container = styled.div`
   // border: 2px solid green;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 30px;
   margin: 3rem 0;
+
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 540px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 320px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 const Card = styled.div`
-  // border: 1px solid yellow;
+  margin: 0 auto;
   padding: 15px;
   transition: all 0.3s ease-in-out;
   border-radius: 15px;
@@ -100,14 +55,41 @@ const Card = styled.div`
     box-shadow: 0 0 1px;
     transform: scale(1.1);
   }
+  @media (max-width: 768px) {
+    width: 14rem;
+  }
+
+  @media (max-width: 414px) {
+    width: 11rem;
+  }
+  @media (max-width: 375px) {
+    width: 10rem;
+  }
 `;
 
 const List = styled(Link)`
   text-decoration: none;
   color: currentColor;
+
+  p {
+    font-size: 1rem;
+    @media (max-width: 414px) {
+      font-size: 0.8rem;
+    }
+  }
 `;
 
 const Img = styled.img`
   width: 17rem;
   border-radius: 15px;
+
+  @media (max-width: 768px) {
+    width: 12rem;
+  }
+  @media (max-width: 414px) {
+    width: 9rem;
+  }
+  @media (max-width: 375px) {
+    width: 8rem;
+  }
 `;
