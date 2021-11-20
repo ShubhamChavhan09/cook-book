c03e3169a4d2d695d5ed59efac07df61318bf452
Shubham <shubh.chavhan00@gmail.com>
2021-11-20 21:24:45 +0530

    Changed layout and added styling

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -22,13 +22,12 @@ const Categories = () => {
   return (
     <Container>
       {Object.values(types).map((type) => (
-        <div key={type.idCategory}>
-          <LinkItem to={`/categories/${type.strCategory}`}>
+        <LinkItem key={type.idCategory} to={`/categories/${type.strCategory}`}>
+          <Card>
             <img src={type.strCategoryThumb} alt={type.strCategory} />
-            <h2>{type.strCategory}</h2>
-          </LinkItem>
-          {/* <p>{type.strCategoryDescription}</p> */}
-        </div>
+          </Card>
+          <p>{type.strCategory}</p>
+        </LinkItem>
       ))}
     </Container>
   );
@@ -36,47 +35,82 @@ const Categories = () => {
 
 export default Categories;
 
+// const Container = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(5, 1fr);
+//   gap: 50px;
+//   text-align: center;
+//   font-size: 12px;
+//   padding: 40px;
+
+//   h2 {
+//     margin: 0;
+//   }
+//   img {
+//     width: 150px;
+//     aspect-ratio: 1/1;
+//     border-radius: 50%;
+//     outline: 1px solid;
+//     outline-offset: 6px;
+//     transition: all 0.1s linear;
+
+//     :hover {
+//       outline: 1px dashed;
+//       transform: scale(1.03);
+//     }
+
+//     @media (max-width: 500px) {
+//     }
+//   }
+
+//   @media (max-width: 980px) {
+//     grid-template-columns: repeat(3, 1fr);
+//   }
+//   @media (max-width: 700px) {
+//     grid-template-columns: repeat(2, 1fr);
+//   }
+//   @media (max-width: 450px) {
+//     grid-template-columns: repeat(1, 1fr);
+//   }
+// `;
+
+const LinkItem = styled(Link)`
+  font-size: 1.2rem;
+  text-align: left;
+  text-decoration: none;
+  margin: 0 auto;
+  border-radius: 10px;
+  color: ${({ theme }) => theme.text};
+  p {
+    margin: 0 0 0 15px;
+  }
+  :hover > div {
+    // transform: scale(1.1);
+    box-shadow: 0 0 4px;
+
+    transition: all 0.3s ease;
+  }
+`;
 const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  gap: 50px;
-  text-align: center;
-  font-size: 12px;
-  padding: 40px;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
 
-  h2 {
-    margin: 0;
-  }
-  img {
-    width: 150px;
-    aspect-ratio: 1/1;
-    border-radius: 50%;
-    outline: 1px solid;
-    outline-offset: 6px;
-    transition: all 0.1s linear;
-
-    :hover {
-      outline: 1px dashed;
-      transform: scale(1.03);
-    }
-
-    @media (max-width: 500px) {
-    }
-  }
-
-  @media (max-width: 980px) {
+  @media (max-width: 1024px) {
     grid-template-columns: repeat(3, 1fr);
-  }
-  @media (max-width: 700px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (max-width: 450px) {
-    grid-template-columns: repeat(1, 1fr);
   }
 `;
 
-const LinkItem = styled(Link)`
-  text-decoration: none;
+const Card = styled.div`
+  border: ${({ theme }) => `1px solid ${theme.border}`};
+  // box-shadow: 0 0 4px;
+  border-radius: 20px;
+  width: 296px;
+  height: 185px;
+  overflow: hidden;
 
-  color: ${({ theme }) => theme.text};
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `;
