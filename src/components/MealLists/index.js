256f9887f245de2c0593af936108a673df57fc2e
Shubham <shubh.chavhan00@gmail.com>
2021-11-24 17:45:08 +0530

    Made mobile responsive

--- src/components/MealLists/index.js
+++ src/components/MealLists/index.js
@@ -13,9 +13,9 @@ const MealLists = ({ meals }) => {
               <List to={`/${meal.strMeal}/${meal.idMeal}`}>
                 <Img src={meal.strMealThumb} alt={meal.strMeal} />
                 <p>{meal.strMeal}</p>
-                <p>
+                <em>
                   {meal.strArea && meal.strCategory ? `${meal.strArea}` : null}
-                </p>
+                </em>
               </List>
             </Card>
           ))}
@@ -28,64 +28,50 @@ export default MealLists;
 
 const Container = styled.div`
   // border: 2px solid green;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 30px;
-  margin: 0 auto;
-  padding: 3rem 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  // padding: 3rem 0;
 
-  @media (max-width: 1024px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-  @media (max-width: 540px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (max-width: 320px) {
-    grid-template-columns: repeat(1, 1fr);
+  @media (max-width: 414px) {
+    padding: 0;
   }
 `;
 
 const Card = styled.div`
-  margin: 0 auto;
-  padding: 0.5rem;
+  margin: 1rem;
+  // padding: 0.5rem;
   transition: all 0.3s ease-in-out;
   border-radius: 15px;
   text-align: left;
   border: ${({ theme }) => `1px solid ${theme.border}`};
-  // width: 18rem;
-  
+  width: 11rem;
 
   :hover {
-    transform: scale(1.1);
+    transform: scale(1.02);
     box-shadow: 0 0 1px;
-    background: ${({ theme }) => ` ${theme.body}`} ;
-  
-  @media (max-width: 768px) {
-    width: 14rem;
+    // background: ${({ theme }) => ` ${theme.body}`};
   }
 
+  @media (max-width: 768px) {
+    width: 8rem;
+  }
   @media (max-width: 414px) {
+    width: 10.9rem;
+    margin: 0.5rem;
+  }
+  @media (max-width: 411px) {
     width: 10.5rem;
-    
+    margin: 0.5rem;
   }
   @media (max-width: 375px) {
-    width: 7rem;
+    width: 9rem;
   }
 `;
 
 const Img = styled.img`
   border-radius: 15px;
   width: 100%;
-
-  // @media (max-width: 768px) {
-  //   width: 13rem;
-  // }
-  // @media (max-width: 414px) {
-  //   width: 9.5rem;
-  // }
-  // @media (max-width: 375px) {
-  //   width: 8rem;
-  // }
 `;
 
 const List = styled(Link)`
@@ -93,20 +79,15 @@ const List = styled(Link)`
   color: currentColor;
   width: 17rem;
 
-  @media (max-width: 768px) {
-    width: 13rem;
-  }
-  @media (max-width: 414px) {
-    width: 9.5rem;
-  }
-  @media (max-width: 375px) {
-    width: 8rem;
-  }
+  p,
+  em {
+    font-size: 0.9rem;
 
-  p {
-    font-size: 1rem;
     @media (max-width: 414px) {
-      font-size: 0.7rem;
+      font-size: 0.8rem;
+    }
+    @media (max-width: 768px) {
+      font-size: 0.8rem;
     }
   }
 `;
