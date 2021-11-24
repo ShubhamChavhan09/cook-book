d8f21e6429e6a03bf415089e6c15b11fb6337068
Shubham <shubh.chavhan00@gmail.com>
2021-11-24 17:42:03 +0530

    Changed background image and added Github link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,60 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import SearchMeal from "../components/SearchMeal";
-import { FaBars } from "react-icons/fa";
-import { AiOutlineClose } from "react-icons/ai";
-import { NavLink } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
 
 const Home = () => {
-  const [sidebar, setSidebar] = useState(false);
-
-  const showSidebar = () => setSidebar(!sidebar);
-
   return (
     <Div>
-      <SideBar>
-        <div>
-          <FaBars style={{ color: "currentColor" }} onClick={showSidebar} />
-        </div>
-
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul onClick={showSidebar}>
-            <AiOutlineClose />
-            <li>
-              <ListItem to="/">Home</ListItem>
-            </li>
-            <li>
-              <ListItem to="/categories">Categories</ListItem>
-            </li>
-            <li>
-              <ListItem to="/favourites">Favourites</ListItem>
-            </li>
-            <li>
-              <ListItem to="/randomMeal">Random</ListItem>
-            </li>
-          </ul>
-        </nav>
-      </SideBar>
-
+      <Git
+        href="https://github.com/ShubhamChavhan09/meal-recipes-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGithub />
+      </Git>
       <Flex>
         <h2>Discover the best food recipe.</h2>
         <SearchMeal />
         {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
-            // fill="rgba(214, 48, 49,1.0)"
-            fill="#610C04"
+            fill="white"
             fillOpacity="1"
-            d="M0,128L120,154.7C240,181,480,235,720,261.3C960,288,1200,288,1320,288L1440,288L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+            d="M0,64L120,96C240,128,480,192,720,186.7C960,181,1200,107,1320,69.3L1440,32L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
           ></path>
         </svg> */}
 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
-            fill="currentColor"
+            fill="rgba(200, 214, 229,0.75)"
             fillOpacity="1"
-            d="M0,64L120,96C240,128,480,192,720,186.7C960,181,1200,107,1320,69.3L1440,32L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+            d="M0,96L120,122.7C240,149,480,203,720,234.7C960,267,1200,277,1320,282.7L1440,288L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
           ></path>
         </svg>
+        <div></div>
       </Flex>
     </Div>
   );
@@ -65,9 +42,13 @@ export default Home;
 const Div = styled.div``;
 
 const Flex = styled.div`
-  background-image: url('https://images.unsplash.com/photo-1507048331197-7d4ac70811cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80') ;
+  // background-image: url('https://images.unsplash.com/photo-1507048331197-7d4ac70811cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80') ;
+  // background-image: url('https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80') ;
+  // background-image: url('https://images.unsplash.com/photo-1608835291093-394b0c943a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80') ;
+  // background-image: url('https://images.unsplash.com/photo-1592417817038-d13fd7342605?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80') ;
+  background-image: url('https://images.unsplash.com/photo-1605522469906-3fe226b356bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80') ;
   background-repeat: no-repeat;
-  background-color: rgba(0,0,0,0.7);
+  background-color: rgba(0,0,0,0.5);
   background-size: cover; 
   background-blend-mode: multiply;
   background-position: center;
@@ -80,11 +61,16 @@ const Flex = styled.div`
   flex-direction: column;
   height: 100vh;
    overflow: hidden;
+   padding: 1rem;
+
+
+   
+
 
 h2{
-  margin-top: 150px;
-color: #ffffff;
-font-size: 3rem;
+  margin-top: 10rem;
+ color: #ffffff;
+ font-size: 3rem;
 font-weight: 400;
 
 @media (max-width: 414px){
@@ -96,36 +82,26 @@ font-weight: 400;
 
 svg{
   position: absolute;
-  bottom: -110px;
+  bottom: -60px;
+  right:0;
+  
   @media (max-width: 414px){
     bottom: 0;
   }
 }
 
-  `;
-
-//
-
-const SideBar = styled.div`
-  z-index: 1;
-  display: none;
-  width: 250px;
-  ul {
-    padding: 0;
-  }
-  li {
-    list-style-type: none;
-    font-size: 1.1rem;
-    margin: 40px 0;
-  }
-
-  // @media (max-width: 800px) {
-  //   display: flex;
-  // }
 `;
 
-const ListItem = styled(NavLink)`
-  margin: 0 40px;
-  text-decoration: none;
-  color: currentColor;
+const Git = styled.a`
+  font-size: 2.2rem;
+  position: fixed;
+  color: #0d0d0d;
+  left: 2rem;
+  bottom: 1rem;
+
+  @media (max-width: 800px) {
+    font-size: 1.5rem;
+    left: 0.9rem;
+    bottom: 0.3rem;
+  }
 `;
