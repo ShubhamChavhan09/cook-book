e96189c2bd0f06e924d557b3c2c58c7985291425
Shubham <shubh.chavhan00@gmail.com>
2021-11-28 08:35:57 +0530

    Added display: flex to the page

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { useStore } from "../context";
 import MealLists from "../components/MealLists";
+import styled from "styled-components";
 
 const Favourites = () => {
   const { favourites } = useStore();
 
   return (
-    <>
+    <Container>
       {favourites.length ? (
         <MealLists meals={favourites} />
       ) : (
         <p>No Favourites</p>
       )}
-    </>
+    </Container>
   );
 };
 
 export default Favourites;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
